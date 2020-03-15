b70a25bb210a908580813b9e88ca2f3f0b55c8f2
ShunjiYamanaka <syun.singernet@gmail.com>
2020-03-15 12:10:36 +0900

    住所録アプリケーション開発
    モデルクラスの実装、コントローラーの実装

---
+++ AddressBookApp/Models/AddressMetadata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AddressBookApp.Models
+{
+    //partialより、ｸﾗｽ・構造体・ﾒｿｯﾄﾞ　複数のソースファイルで分割できる
+    [MetadataType(typeof(AddressMetadata))]
+    public partial class Address 
+    {
+
+    }
+
+    public class AddressMetadata
+    {
+        //AddressBookModelと一致するﾌﾟﾛﾊﾟﾃｨと一致しているものを操作できる
+        //DBファーストの開発ではMetadataｸﾗｽより操作する必要がある
+        [Required]
+        [DisplayName("氏名")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("カナ")]
+        public string Kana { get; set; }
+
+        [DisplayName("郵便番号")]
+        public string ZipCode { get; set; }
+
+        [DisplayName("都道府県")]
+        public string Prefecture { get; set; }
+
+        [DisplayName("住所")]
+        public string StreetAddress { get; set; }
+
+        [DisplayName("電話番号")]
+        public string Telephone { get; set; }
+
+        [DisplayName("メール")]
+        public string Mail { get; set; }
+
+        [DisplayName("グループ")]
+        public Nullable<int> Group_Id { get; set; }
+    }
+}
